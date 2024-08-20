b458e46800fa6719aac59f581bbc532c9bdfee0c
abderahmenabdou <ettaharmammar@outlook.com>
2024-08-20 08:10:46 +0100

    Kasper Template Layout

---
+++ main.js
@@ -0,0 +1,49 @@
+let slideImages = Array.from(document.querySelectorAll("main img"));
+let bullets = Array.from(document.querySelectorAll(".bullet li"));
+let previous = document.getElementById("previous");
+let next = document.getElementById("next");
+let current = 1;
+
+previous.onclick = previousImage;
+next.onclick = nextImage;
+
+function previousImage() {
+  if (current > 1) {
+    current--;
+    updateImage();
+  }
+}
+function nextImage() {
+  if (current < slideImages.length) {
+    current++;
+    updateImage();
+  }
+}
+
+function updateImage() {
+  removeAll();
+  slideImages[current - 1].classList.add("active");
+  bullets[current - 1].classList.add("active");
+
+  previous.classList.toggle("disable", current === 1);
+  next.classList.toggle("disable", current === slideImages.length);
+}
+
+bullets.forEach(function (bullet, index) {
+  bullet.onclick = function () {
+    current = index + 1;
+    updateImage();
+  };
+});
+
+function removeAll() {
+  slideImages.forEach(function (img) {
+    img.classList.remove("active");
+  });
+
+  bullets.forEach(function (bullet) {
+    bullet.classList.remove("active");
+  });
+}
+
+updateImage();
